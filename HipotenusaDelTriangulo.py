beefd144e5d05aa75aa97c81f29b906949935040
Guiller022005 <118319910+Guiller022005@users.noreply.github.com>
2024-03-01 14:52:22 -0500

    HipotenusaSencilla

--- HipotenusaDelTriangulo.py
+++ HipotenusaDelTriangulo.py
@@ -1,6 +1,8 @@
 # Hipotenusa del triangulo
+import math
 cat_a= float(input("Ingresa el cateto a"))
 cat_b= float(input("Ingresa el cateto b"))
 
-hipo_c = (cat_a** + cat_b**2)**(1/2)
-print("La hipotenusa del triangulo es ", hipo_c)
+hipo_c = (cat_a**2 + cat_b**2)
+hipo_2 =  math.sqrt(hipo_c)
+print("La hipotenusa del triangulo es ", hipo_2)
