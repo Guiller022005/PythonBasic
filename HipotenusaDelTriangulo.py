017c8467eee777138f00f654afa002b18e7d4022
Guiller022005 <118319910+Guiller022005@users.noreply.github.com>
2024-03-01 14:29:51 -0500

    HallaLaHipotenusaDelTriangulo

---
+++ HipotenusaDelTriangulo.py
@@ -0,0 +1,6 @@
+# Hipotenusa del triangulo
+cat_a= float(input("Ingresa el cateto a"))
+cat_b= float(input("Ingresa el cateto b"))
+
+hipo_c = (cat_a** + cat_b**2)**(1/2)
+print("La hipotenusa del triangulo es ", hipo_c)
