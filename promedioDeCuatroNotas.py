e705215c6f9986db626bcc600bb5b75250d9dc19
Guiller022005 <118319910+Guiller022005@users.noreply.github.com>
2024-03-01 13:55:28 -0500

    PromedioNotas

---
+++ promedioDeCuatroNotas.py
@@ -0,0 +1,6 @@
+nota_1 = int(input("Ingresa la primer nota"))
+nota_2 = int(input("Ingresa la segunda nota"))
+nota_3 = int(input("Ingresa la tercer nota"))
+nota_4 = int(input("Ingresa la cuarta nota"))
+promedio = float((nota_1 + nota_2 + nota_3 + nota_4)/4)
+print("El promedio de tus notas es ", promedio)
