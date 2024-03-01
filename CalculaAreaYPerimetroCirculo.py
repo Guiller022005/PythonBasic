88e4d9c17c1b98cc7f6e7600fb473556a3cb2a9c
Guiller022005 <118319910+Guiller022005@users.noreply.github.com>
2024-03-01 13:47:13 -0500

    CirculoAreaYPerimetro

---
+++ CalculaAreaYPerimetroCirculo.py
@@ -0,0 +1,5 @@
+radio = int(input("Ingresa el area de un circulo"))
+perimetro = 3.1416 * 2 * radio
+area = 3.1416 * radio * radio
+print("El area del circulo es", area)
+print("El perimetro del circulo es", perimetro)
