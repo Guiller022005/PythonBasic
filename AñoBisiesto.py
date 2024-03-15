8b3c2e4273d7067a62d5b6de0f12be201575ffd0
Guiller022005 <118319910+Guiller022005@users.noreply.github.com>
2024-03-15 13:32:06 -0500

    feat: :sparkles: Determinar si un año es o no bisiesto
    comprobamos q el año sea divisible por 4, 100, 400, si cumple las condiciones es verdadero si no es falso

---
+++ AñoBisiesto.py
@@ -0,0 +1,20 @@
+año = int(input("Ingresa un año: "))
+def año_bisiesto(año):
+    if (año / 4) == 0:  # Si es divisible por 4
+        if (año % 100) == 0:  # Si es divisible por 100
+            if (año % 400) == 0:  # Si es divisible por 400
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+    
+if año_bisiesto(año):
+    print(f"{año} es bisiesto")
+else:
+    print(f"{año} no es bisiesto")
+
+
