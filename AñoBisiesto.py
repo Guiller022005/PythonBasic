c537fa1381596d4aa6be7c0f4dec09203550e014
Guiller022005 <118319910+Guiller022005@users.noreply.github.com>
2024-03-15 13:39:39 -0500

    Mejora de un detalle el de modulo

--- AñoBisiesto.py
+++ AñoBisiesto.py
@@ -1,6 +1,6 @@
 año = int(input("Ingresa un año: "))
 def año_bisiesto(año):
-    if (año / 4) == 0:  # Si es divisible por 4
+    if (año % 4) == 0:  # Si es divisible por 4
         if (año % 100) == 0:  # Si es divisible por 100
             if (año % 400) == 0:  # Si es divisible por 400
                 return True
