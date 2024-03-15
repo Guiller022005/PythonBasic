715857ea9939da332c11d30244833d3de8fc7114
Guiller022005 <118319910+Guiller022005@users.noreply.github.com>
2024-03-15 13:56:13 -0500

    feat: :sparkles: Algoritmo para ordenar los numeros
    Pide al usuario q ingrese 4 numeros y luego los ordeno en una lista

---
+++ OrdenarlosNumeros.py
@@ -0,0 +1,8 @@
+num1 = int(input("Ingresa un numero "))
+num2 = int(input("Ingresa un numero "))
+num3 = int(input("Ingresa un numero "))
+num4 = int(input("Ingresa un numero "))
+
+order = sorted([num1, num2, num3, num4])
+
+print("El orden de los numeros ingresados es ", order)
