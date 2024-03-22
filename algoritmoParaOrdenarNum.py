b985853de71828b74a3232175ff7e51f1bf5a6cb
Guiller022005 <118319910+Guiller022005@users.noreply.github.com>
2024-03-22 06:48:42 -0500

    feat: :construction: Algoritmo para ordenar numeros de menor a mayor
    algoritmo para ordenar de dos y hasta 4 numeros

---
+++ algoritmoParaOrdenarNum.py
@@ -0,0 +1,22 @@
+#Programa q reciba como entrada dos números, y los muestre ordenados de menor a mayor:
+
+print("\nOrdenar dos numeros")
+Numeros = [int(input("Ingresa el primer numero: ")), int(input("Ingresa el segundo numero: "))]
+print(Numeros.sort())
+print(f"\nLos Numeros ingresados ordenados de menor a mayor son: {Numeros}")
+
+#Programa q reciba como entrada tres números, y los muestre ordenados de menor a mayor:
+
+print("\nOrdenar tres numeros")
+
+Numeros = [int(input("Ingresa el primer numero: ")), int(input("Ingresa el segundo numero: ")), int(input("Ingresa el tercer numero: ")) ]
+print(Numeros.sort())
+print(f"\nLos Numeros ingresados ordenados de menor a mayor son: {Numeros}")
+
+#Programa q reciba como entrada cuatro números, y los muestre ordenados de menor a mayor:
+
+print("\nOrdenar cuatro numeros")
+
+Numeros = [int(input("Ingresa el primer numero: ")), int(input("Ingresa el segundo numero: ")),int(input("Ingresa el tercer numero: ")), int(input("Ingresa el cuarto numero: ")) ]
+print(Numeros.sort())
+print(f"\nLos Numeros ingresados ordenados de menor a mayor son: {Numeros}")
